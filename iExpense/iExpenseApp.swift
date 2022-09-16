8235280bebeaee7d49bf52007e290b81adfaa9c7
Carlossiii <cv5673@gmail.com>
2022-09-16 11:35:39 -0300

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Carlos Vinicius on 16/09/22.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
