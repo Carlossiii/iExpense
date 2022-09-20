16179398173e5e733ca45ecfd9dcbea1794eff8f
Carlossiii <cv5673@gmail.com>
2022-09-20 17:14:40 -0300

    Entire project done, just minor tasks to complete.

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,15 +7,69 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+struct ExpenseStyle: ViewModifier {
+    let expenseItem: ExpenseItem
+
+    func body(content: Content) -> some View {
+        switch expenseItem.amount {
+        case 0..<10:
+            content
+                .foregroundColor(.green)
+        case 10..<100:
+            content
+                .foregroundColor(.blue)
+        default:
+            content
+                .font(.headline)
+                .foregroundColor(.red)
         }
-        .padding()
+    }
+}
+
+extension View {
+    func expenseStyle(for expenseItem: ExpenseItem) -> some View {
+        modifier(ExpenseStyle(expenseItem: expenseItem))
+    }
+}
+
+struct ContentView: View {
+    @StateObject var expenses = Expenses()
+    @State private var showingAddExpense = false
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(expenses.items) { item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.name)
+                                .font(.headline)
+                            Text(item.type)
+                        }
+                        Spacer()
+                        
+                        Text(item.amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                            .expenseStyle(for: item)
+                    }
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationTitle("iExpense")
+            .toolbar {
+                Button {
+                    showingAddExpense = true
+                } label: {
+                    Image(systemName: "plus")
+                }
+            }
+            .sheet(isPresented: $showingAddExpense) {
+                AddView(expenses: expenses)
+            }
+        }
+    }
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
     }
 }
 
